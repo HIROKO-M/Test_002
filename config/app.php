67fd86cd2dcd951a2b170815adafb9a0e6dc67db
EC2 Default User <ec2-user@ip-172-31-32-117.ec2.internal>
2020-03-17 03:25:12 +0000

    CRUD messages

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,  // 追加
 
         /*
          * Application Service Providers...
@@ -192,6 +193,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,    //追加
+        'Html' => Collective\Html\HtmlFacade::class,    //追加
 
     ],
 
